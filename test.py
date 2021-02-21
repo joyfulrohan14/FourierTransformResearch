2d7099ce7b805a13beb07f2180801f0409ca0302
Rohan Ray <rayra@vcu.edu>
2021-02-21 18:17:12 -0500

    new changes

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from matplotlib.widgets import Slider, Button, TextBox
 
 print("Enter Time Increment for all four periods")
 #time = input("Enter your value: ")
@@ -33,9 +34,34 @@ print("Enter Frequency increment")
 frequ = 1.5 #float(input("Enter your value: "))
 print("Enter Phase Dispersion(radians)")
 phase = 1.57 #float(input("Enter your value: "))
+print("Enter Integral Correction")
+cor = 0.087 #float(input("Enter your value: "))
+print("Enter Integration Scale")
+scale = 0.08 #float(input("Enter your value: "))
+print("Enter Integration Shift")
+shift = 1 #float(input("Enter your value: "))
+print("Enter Integral Correction")
+cor1 = 0.07#float(input("Enter your value: "))
+print("Enter Integration Scale")
+scale1 = 0.05 #float(input("Enter your value: "))
+print("Enter Integration Shift")
+shift1 = 0.1 #float(input("Enter your value: "))
+
+#plt.ion()
+fig, axs = plt.subplots(nrows=5, ncols=2)
 
 
-# Some example data to display
+#slider 
+#axfreq = plt.axes([0.25, 0.1, 0.65, 0.03])
+#sfreq = Slider(axfreq, 'time', valmin = 0.0, valmax= 0.1, valinit=time, valstep=0.00055)
+
+#button
+#resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+#button = Button(resetax, 'Reset', hovercolor='0.975')
+
+#textbox
+
+
 x = np.arange(0,time*636,time)
 y1 = (np.cos(x*3.14*2*frequency))*intensity
 y2 = (np.cos(x*3.14*2*frequency1))*intensity1
@@ -48,6 +74,7 @@ freqRad = freq/(2*3.1415927)
 y8=np.array([])
 y9=np.array([])
 y10=np.array([])
+y12 = np.array([])
 tot=0
 for b in freq:
     y7=y5*np.cos(b*x)*time
@@ -67,10 +94,28 @@ for b in freq:
         tot = tot + v
     y10=np.append(y10, [tot])
     tot=0
-    #=-$AD3*SIN(AX$1*$A3+$E$8)*$C$8
+last = 0
+for b in y8:
+    calc = b +last -cor
+    last=calc
+    y12=np.append(y12, [calc])
+y13 = (y12/np.max(y8))*scale
+y14 = y13+shift
+y11=np.sqrt(y8**2+y10**2)
+y15 = np.array([])
+for b in y11:
+    calc = b +last -cor1
+    last=calc
+    y15=np.append(y15, [calc])
+y16 = (y15/np.max(y11))*scale1
+y17 = y16+shift1
+    
+
+
 f = open("absorption.txt", "w")
 t = open("frequecy.txt", "w")
 v = open("phased.txt", "w")
+p = open("integral.txt", "w")
 for y in y8:
     f.write(str(y))
     f.write('\n')
@@ -82,17 +127,23 @@ t.close()
 for y in y10:
     v.write(str(y))
     v.write('\n')
-v.close()  
-# print(len(y8))
-# print(len(freqRad))   
-# print(freqRad.dtype) 
-# print(y8.dtype)    
+v.close() 
+for y in y12:
+    p.write(str(y))
+    p.write('\n')
+p.close()   
 
 
-#=$AD2*COS(AX$1*$A2)*$C$8
+
+#axbox = fig.add_axes([0.1, 0.05, 0.8, 0.075])
+#text_box = TextBox(axbox, "Enter Phase Shift")
+#def submit(value):
+#    global phase = value
+#    makePlots()
+#    plt.draw()
+#text_box.on_submit(submit)
 
 
-fig, axs = plt.subplots(nrows=5, ncols=2)
 fig.tight_layout() 
 axs[0, 0].plot(x, y1)
 axs[0, 0].set_title('Period 1')
@@ -109,11 +160,16 @@ axs[2, 0].set_title('FID period 1')
 axs[3, 0].plot(freqRad, y8, 'tab:blue')
 axs[3, 0].set_title('Absorption Spectrum')
 axs[3, 1].plot(freqRad, y9, 'tab:blue')
+axs[3, 0].plot(freqRad, y14, 'tab:orange')
 axs[3, 1].set_title('Dispersion Spectrum')
 axs[4, 0].plot(freqRad, y10, 'tab:orange')
 axs[4, 0].set_title('Phased Dispersion')
+axs[4, 1].plot(freqRad, y11, 'tab:orange')
+axs[4, 1].plot(freqRad, y17, 'tab:red')
+axs[4, 1].set_title('Combined Phase Absorption and Dispersion')
 
 #Label x and y plots
+axs[4,1].set(xlabel='Frequency(Hz)', ylabel='Intensity(A.U.)')
 axs[4,0].set(xlabel='Frequency(Hz)', ylabel='Intensity(A.U.)')
 axs[3,0].set(xlabel='Frequency(Hz)', ylabel='Intensity(A.I.)')
 axs[3,1].set(xlabel='Frequency(Hz)', ylabel='Intensity(A.U.)')
@@ -128,5 +184,9 @@ for ax in axs.flat:
     ax.axhline(y=0, color='k')
     ax.axvline(x=0, color='k')
     ax.grid(True, which='both')
+
+#def reset(event):
+#    sfreq.reset()
+#button.on_clicked(reset)
 
 plt.show()
